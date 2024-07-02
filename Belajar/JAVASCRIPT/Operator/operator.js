b8cedf6b90ca1a11f138266242625abe7007b4c1
Andri Maulana Ramadhan <andrimaulanaramadhan54@gmail.com>
2024-07-02 22:11:08 +0700

    02/07/2024

---
+++ Belajar/JAVASCRIPT/Operator/operator.js
@@ -0,0 +1,42 @@
+console.log(5+5);
+console.log(5-5);
+console.log(5*5);
+console.log(5/5);
+console.log(5%5);
+console.log((1+2)*3-4);
+console.log(1+2*3-4);
+
+console.log(x=5);
+console.log(y=5);
+console.log(x += y);
+console.log(x -= y);
+console.log(x *= y);
+console.log(x /= y);
+console.log(x %= y);
+
+console.log(5 == 5);
+console.log(5 == "5");
+console.log(5 != 5);
+console.log(5 != "5");
+console.log(5 == 6);
+console.log(5 === 5);
+console.log(5 === "5");
+console.log(5 !== 5);
+console.log(5 !== "5");
+console.log(5 <= 5);
+console.log(5 >= 5);
+
+console.log(5 == 5) && (10 ==="10");
+console.log(5 == 5) && (10 === 10);
+console.log(5 == 5) || (10 === "10");
+console.log(!5 == 5);
+
+console.log("Apple" + " " + "Pen");
+console.log("10" + 10 + 10);
+console.log(10 + 10 + "10");
+
+console.log(typeof(10));
+console.log(typeof("Andri"));
+console.log(typeof(true));
+
+console.log((4 % 2 == 0) ? "Genap" : "Ganjil");
