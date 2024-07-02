df3a7b3889bbbaf429a42bc1548090b566c67f9a
Andri Maulana Ramadhan <andrimaulanaramadhan54@gmail.com>
2024-07-02 20:03:36 +0700

    02/07/2024

---
+++ Belajar/JAVASCRIPT/Angka/angka.js
@@ -0,0 +1,13 @@
+console.log(10);
+console.log(10.45);
+console.log(-10);
+console.log(0.12);
+
+console.log(10 + 10);
+console.log(0.7 + 0.3);
+console.log(0.2 + 0.1);
+
+console.log(100 / 0);
+console.log(-100 / 0);
+console.log(100 / "Andri");
+console.log(100 / "10");
